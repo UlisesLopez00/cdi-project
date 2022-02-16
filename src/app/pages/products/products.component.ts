8283aed66c11558a21ba8eb8d480ec0cd335dda2
omarrmz13 <omar.wst13@gmail.com>
2022-02-16 13:36:18 -0600

    Formulario Item Comleto

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -3,13 +3,38 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {}
+  public alerta:any = {}
+  public cdb: any = '';
+  public descripcion: string = '';
+  public precio?: number;
+  public cantidad?: number;
+  ngOnInit(): void {}
+  public focus(id: any) {
+    document.getElementById(id)?.focus();
+    console.log('hola');
   }
 
+
+
+  public registrar(){
+    let data ={
+      cdb:this.cdb,
+      descripcion:this.descripcion,
+      precio:this.precio,
+      cantidad:this.cantidad
+    }
+    if (!this.cdb || !this.descripcion || !this.precio || !this.cantidad) {
+      console.log("Error");
+      
+      this.alerta={
+        show:true,
+        msg:'Todos los campos son obligatorios',
+        color:'#FFCC00'
+      }
+    }
+  }
 }
