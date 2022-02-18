4ac592358479338ef83bfef7ba23285c6386f2c5
omarrmz13 <omar.wst13@gmail.com>
2022-02-18 09:28:32 -0600

    Responsivo Menu

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   constructor() {}
+  public showMenuMovil:boolean = false;
   public routes = [
     {
       name:'Dashboard',
