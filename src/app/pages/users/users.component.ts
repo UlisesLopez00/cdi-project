3ed8aefad630f1765f3f8bd8dc5fd2b8708599e1
omarrmz13 <omar.wst13@gmail.com>
2022-02-21 15:47:46 -0600

    Editar Users

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -4,96 +4,112 @@ import { UsersService } from '../../services/users.service';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
 export class UsersComponent implements OnInit {
-public name: string = '';
-public email: string='';
-public pwd: string='';
-public confirmPwd: string = '';
-public alerta:any = {};
-public users:any = [];
+  public name: string = '';
+  public email: string = '';
+  public pwd: string = '';
+  public confirmPwd: string = '';
+  public alerta: any = {};
+  public users: any = [];
+  public editar: boolean = false;
 
-  constructor(private userService: UsersService) { }
+  constructor(private userService: UsersService) {}
 
   ngOnInit() {
     this.showUsers();
   }
 
-  showUsers(){
+  showUsers() {
     this.userService.userGet().subscribe({
-      next: (data:any)=>{
-        this.users = data.data
+      next: (data: any) => {
+        this.users = data.data;
       },
-      error:(err:any)=>{
+      error: (err: any) => {
         this.alerta = {
-          show:true,
-          msg:'Error al conectar con el servidor. Intentelo mas tarde',
-          icon:'error',
-          color:'red'
-        }
-        console.log(err)
-      }
-    })
+          show: true,
+          msg: 'Error al conectar con el servidor. Intentelo mas tarde',
+          icon: 'error',
+          color: 'red',
+        };
+        console.log(err);
+      },
+    });
   }
 
-  registerUser(){
-    let data = { 
+  registerUser() {
+    let data = {
       user: this.name,
       email: this.email,
-      pwd: this.pwd
+      pwd: this.pwd,
     };
-   
-    if (this.confirmPwd===this.pwd) {
+
+    if (this.confirmPwd === this.pwd) {
       this.userService.userPost(data).subscribe({
-        next: (data:any)=>{
+        next: (data: any) => {
           this.alerta = {
-            show:true,
-            msg:'Usuario Registrado con éxito',
-            color:'green'
-          }
+            show: true,
+            msg: 'Usuario Registrado con éxito',
+            color: 'green',
+          };
           this.showUsers();
         },
-        error: (err: any)=>{
+        error: (err: any) => {
           this.alerta = {
-            show:true,
-            msg:'Error',
-            color:'red'
-          }
-          console.log(err)
-        } 
-      })
+            show: true,
+            msg: 'Error',
+            color: 'red',
+          };
+          console.log(err);
+        },
+      });
     } else {
       this.alerta = {
-        show:true,
-        msg:'Las contraseñas no coinciden',
-        color:'red',
-        icon:'warning'
-      }
+        show: true,
+        msg: 'Las contraseñas no coinciden',
+        color: 'red',
+        icon: 'warning',
+      };
       console.log('contraseña !=');
     }
   }
 
-  deleteUser(id:any){
-    if(confirm("Seguro de eliminar?"))
-    this.userService.userDelete(id).subscribe({
-      next:(data:any)=>{
-        this.alerta = {
-          show:true,
-          msg:'Usuario eliminado con éxito',
-          color:'green'
-        }
-        this.showUsers();
-      },
-      error: (err:any)=>{
-        this.alerta = {
-          show:true,
-          msg:'Error al eliminar',
-          color:'green'
-        }
-        console.log(err);
-      }
-    })
+  deleteUser(id: any) {
+    if (confirm('Seguro de eliminar?'))
+      this.userService.userDelete(id).subscribe({
+        next: (data: any) => {
+          this.alerta = {
+            show: true,
+            msg: 'Usuario eliminado con éxito',
+            color: 'green',
+          };
+          this.showUsers();
+        },
+        error: (err: any) => {
+          this.alerta = {
+            show: true,
+            msg: 'Error al eliminar',
+            color: 'green',
+          };
+          console.log(err);
+        },
+      });
   }
 
+  clicActualizar(user: any) {
+    this.editar = true;
+    this.name = user.user;
+    this.email = user.email;
+    console.log(user.pwd);
+    
+  }
+  actualizarUser() {
+
+    let data = {
+      user:this.name,
+      email:this.email,
+      pwd:this.pwd
+    };
+  }
 }
