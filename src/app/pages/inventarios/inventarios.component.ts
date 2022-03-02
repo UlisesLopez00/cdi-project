77104b6bcb759d2d440dd116e681fc84fcca0faf
omarrmz13 <omar.wst13@gmail.com>
2022-03-02 08:58:11 -0600

    Show Images in Inventario

--- src/app/pages/inventarios/inventarios.component.ts
+++ src/app/pages/inventarios/inventarios.component.ts
@@ -1,122 +1,132 @@
 import { Component, OnInit } from '@angular/core';
-import { ItemsService } from '../../services/items.service'
+import { ItemsService } from '../../services/items.service';
 
 @Component({
   selector: 'app-inventarios',
   templateUrl: './inventarios.component.html',
-  styleUrls: ['./inventarios.component.css']
+  styleUrls: ['./inventarios.component.css'],
 })
 export class InventariosComponent implements OnInit {
-
-  constructor(private itemService:ItemsService) { }
+  constructor(private itemService: ItemsService) {}
   public alerta: any = {};
   public items: any = '';
   public itemEdit: any = {};
-  public showModalEdit:any = false;
-  public itemSearch:any = '';
-  public itemFound:any = '';
-  public encontrarItem:any = false;
-
+  public showModalEdit: any = false;
+  public itemSearch: any = '';
+  public itemFound: any = '';
+  public encontrarItem: any = false;
+  public imagesView: any = [];
+  public showModalImage:boolean = false;
+  public numberBigImage:number = 0;
   ngOnInit() {
     this.showItems();
   }
 
-  showItems(){
+  showItems() {
     this.itemService.itemGet().subscribe({
-      next: (data:any)=>{
+      next: (data: any) => {
         this.items = data.data;
       },
-      error: (err: any)=>{
-        this.alerta ={
-          show:true,
-          msg:'Error al buscar',
+      error: (err: any) => {
+        this.alerta = {
+          show: true,
+          msg: 'Error al buscar',
           color: 'red',
-          icon:'error'
-        }
-      }
-    })
+          icon: 'error',
+        };
+      },
+    });
   }
 
-  editar(item: any){
+  editar(item: any) {
     this.itemEdit = item;
     console.log(this.itemEdit);
     this.showModalEdit = true;
   }
 
-  cancel(){
+  cancel() {
     this.showModalEdit = false;
   }
 
-  save(id: any){
+  save(id: any) {
     let data = {
       desc: this.itemEdit.desc,
       stock: this.itemEdit.stock,
       price: this.itemEdit.price,
+    };
+    if (confirm('¿Seguro de los cambios?')) {
+      this.itemService.itemPut(data, id).subscribe({
+        next: (data: any) => {
+          this.alerta = {
+            show: true,
+            msg: 'Actualizado con exito',
+            color: 'green',
+            icon: 'success',
+          };
+          this.showItems();
+          this.showModalEdit = false;
+        },
+        error: (err: any) => {
+          this.alerta = {
+            show: true,
+            msg: 'Error al actualizar',
+            color: 'red',
+            icon: 'error',
+          };
+        },
+      });
     }
-    if(confirm('¿Seguro de los cambios?')){
-    this.itemService.itemPut(data,id).subscribe({
-      next: (data:any)=>{
-        this.alerta ={
-          show:true,
-          msg:'Actualizado con exito',
-          color: 'green',
-          icon:'success'
-        }
-        this.showItems();
-        this.showModalEdit = false;
-      },error:(err:any)=>{
-        this.alerta ={
-          show:true,
-          msg:'Error al actualizar',
-          color: 'red',
-          icon:'error'
-        }
-      }
-    })
-  }
   }
 
-  deleteItem(id:any){
+  deleteItem(id: any) {
     if (confirm('¿Seguro de eliminar?')) {
       this.itemService.itemDelete(id).subscribe({
-        next: (data:any)=>{
-          this.alerta ={
-            show:true,
+        next: (data: any) => {
+          this.alerta = {
+            show: true,
             msg: '¡Eliminado con exito!',
             color: 'green',
-            icon:'success'
-          }
+            icon: 'success',
+          };
           this.showItems();
-        },error:(err:any)=>{
-          this.alerta ={
-            show:true,
+        },
+        error: (err: any) => {
+          this.alerta = {
+            show: true,
             msg: 'Error al eliminar',
             color: 'red',
-            icon:'error'
-          }
-        }
-      })
+            icon: 'error',
+          };
+        },
+      });
     }
   }
 
-  searchById(data:any){
+  searchById(data: any) {
     this.itemService.itemGetId(data).subscribe({
-      next:(data:any)=>{        
+      next: (data: any) => {
         this.items = data.data;
-      },error:(err:any)=>{
+      },
+      error: (err: any) => {
         this.alerta = {
-          show:true,
-          msg:'No se encontró el producto',
-          color:'red',
-          icon: 'error'
-        }
-      }
-    })
+          show: true,
+          msg: 'No se encontró el producto',
+          color: 'red',
+          icon: 'error',
+        };
+      },
+    });
   }
 
+  public buscador($event: any) {
+    this.searchById($event);
+  }
 
-  public buscador($event:any){
-    this.searchById($event)
+  public viewImage(image: any) {
+    this.imagesView = image;
     
+    
+    this.showModalImage = true;
+
   }
 }
